b53461f0068591a13c3fc18369e375a84717297d
Yash Kakrecha <yashvk.inexture@gmail.com>
2024-06-10 11:01:40 +0530

    initialized project

---
+++ src/test/java/com/ai/stock_market_prediction/StockMarketPredictionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.stock_market_prediction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketPredictionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
